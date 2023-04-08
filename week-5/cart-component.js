b1efcb6bd2acb76cdd96043abb48a35fc2af2940
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-04-07 21:37:14 -0500

    span tag correction

--- week-5/cart-component.js
+++ week-5/cart-component.js
@@ -5,7 +5,7 @@ export class CartComponent extends HTMLElement {
 }
 
 connectedCallback() {
-    this.innerHTML = '<i id="cart" class="fas fa-shopping-cart"></i> (span id="cart-count"></span>)';
+    this.innerHTML = '<i id="cart" class="fas fa-shopping-cart"></i> <span id="cart-count"></span>)';
 }
 
 customElements.define("cart-component", CartComponent);
