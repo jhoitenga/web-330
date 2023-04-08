d07f313d96530499133c2508a3620b0e991f27e3
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-04-07 21:40:10 -0500

    code cleanup

--- week-5/cart-component.js
+++ week-5/cart-component.js
@@ -2,10 +2,10 @@ export class CartComponent extends HTMLElement {
     constructor() {
         super();
     }
-}
 
 connectedCallback() {
     this.innerHTML = '<i id="cart" class="fas fa-shopping-cart"></i> <span id="cart-count"></span>)';
+}
 }
 
 customElements.define("cart-component", CartComponent);
