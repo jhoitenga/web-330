76f8065d78c5a9cfdb6ca4ecd0e6a0630fb9f64c
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-04-08 20:35:59 -0500

    adding comments

--- week-5/cart-component.js
+++ week-5/cart-component.js
@@ -1,11 +1,15 @@
+// Class that extends JavaScripts HTMLElement object
 export class CartComponent extends HTMLElement {
     constructor() {
+        // Call super() function for the HTMLElement parent object
         super();
     }
 
 connectedCallback() {
+    // Set the innerHTML to a string for the font-awesome shopping-cart icon
     this.innerHTML = '<i id="cartIcon" class="fa fa-shopping-cart"></i> (<span id="cart-count"></span>)';
 }
 }
 
+// Call the customElements.define() function pass-in the string value cart-component and the CartComponent class
 customElements.define("cart-component", CartComponent);
