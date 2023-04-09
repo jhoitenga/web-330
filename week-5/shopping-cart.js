8174ea12d5d9c2964593657c065dfd5fea52c223
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-04-08 20:38:50 -0500

    adding comments

--- week-5/shopping-cart.js
+++ week-5/shopping-cart.js
@@ -1,16 +1,21 @@
 export class ShoppingCart {
     constructor(){
+        // Create a property for products and assign it an empty array
         this.products = [];
     }
 
 count(){
+    // Return the length of the products array
     return this.products.length;
 }
 
+// Single parameter for a product object
 addProduct(product){
+    // In the body of the function, add the product object to the products array
     this.products.push(product);
 }
 
+// Create an iterator, use a for…of statement to loop over the products array. Yield return each iterated product object.
 *[Symbol.iterator]()
 {   
     for(let product of this.products)
