87ddf2a47e975b81141642159354ece2e425bfa6
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-04-08 19:35:08 -0500

    adjusting cart icon

--- week-5/cart-component.js
+++ week-5/cart-component.js
@@ -4,7 +4,7 @@ export class CartComponent extends HTMLElement {
     }
 
 connectedCallback() {
-    this.innerHTML = '<i id="cart" class="fas fa-shopping-cart"></i> <span id="cart-count"></span>)';
+    this.innerHTML = <i id="cartIcon" class="fa fa-shopping-cart"></i> (<span id="cart-count"></span>);
 }
 }
 
