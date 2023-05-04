28ef5369c0030b0d37ecc8308dc8b350ddb754d5
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-05-04 17:02:47 -0500

    updating function

--- week-8/http-client.js
+++ week-8/http-client.js
@@ -1,7 +1,7 @@
 // Create a class named HttpClient
 export class HttpClient{
     // Create an async function named get w/ two parameters: url & params.
-    async function get(url, params = ''){ // Set the params parameter to a default empty string
+    async get(url, params = ''){ // Set the params parameter to a default empty string
         
         // Instantiate a new URL object w/ url param & assign the results to the url parameter
         let urlObj = new URL(url);
