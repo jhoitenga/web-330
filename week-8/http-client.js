1cf915f4f5a47ff7f9d708074840504745defde1
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-05-04 17:11:06 -0500

    updating object

--- week-8/http-client.js
+++ week-8/http-client.js
@@ -4,10 +4,10 @@ export class HttpClient{
     async get(url, params = ''){ // Set the params parameter to a default empty string
         
         // Instantiate a new URL object w/ url param & assign the results to the url parameter
-        let urlObj = new URL(url);
+        let urlObject = new URL(url);
         
         // Instantiate a new URLSearchParams object w/ params parameter & assigning it to the url.search property
-        let paramsObj = new URLSearchParams(params);
+        let paramsObject = new URLSearchParams(params);
         urlObject.search = paramsObject
 
         // Create an object literal named res & fetch the url.toString() variable & request as a GET request
